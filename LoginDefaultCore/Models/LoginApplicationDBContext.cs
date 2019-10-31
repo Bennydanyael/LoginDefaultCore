a77a80c10cfe67f5af09aa916433c8fe4d3ef7df
bennydanyael <bennydanyael@gmail.com>
2019-10-31 10:40:52 +0700

    Add project files.

---
+++ LoginDefaultCore/Models/LoginApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginDefaultCore.Models
+{
+    public class LoginApplicationDBContext : IdentityDbContext
+    {
+        public LoginApplicationDBContext(DbContextOptions<LoginApplicationDBContext> _options):base(_options)
+        {
+        }
+
+        public DbSet<SettingsDataModel> Settings { get; set; }
+    }
+}
